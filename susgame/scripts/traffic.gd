8fb3619a7aaa0b6fa2aa9863d393f521d7d6db72
Malaikah Hafeez <Malaikah Hafeez>
2025-01-31 12:50:20 +0000

    added level 2 framework

---
+++ susgame/scripts/traffic.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+@onready var ray_castright: RayCast2D = $RayCastright
+@onready var ray_castleft: RayCast2D = $RayCastleft
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+const SPEED = 60
+
+var direction = 1
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if ray_castright.is_colliding():
+		direction = -1
+		animated_sprite_2d.flip_h = true
+	if ray_castleft.is_colliding():
+		direction = 1
+		animated_sprite_2d.flip_h = false
+		
+	position.x += direction * SPEED * delta
