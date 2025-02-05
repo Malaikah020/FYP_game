bcd3aceb00a3381e1881923b599858b5a8c0361d
Malaikah Hafeez <Malaikah Hafeez>
2025-02-05 13:25:09 +0000

    level 2 framework playability improved

--- susgame/scripts/game_manager.gd
+++ susgame/scripts/game_manager.gd
@@ -4,6 +4,8 @@ var score = 0
 var level = 1
 var target_points = 20
 
+@onready var mainlabel: Label = $"../Player/Label"
+
 @onready var score_label_1: Label = $score_label1
 @onready var score_label_2: Label = $score_label2
 @onready var score_label_3: Label = $score_label3
@@ -15,12 +17,10 @@ func add_point():
 	score_label_1.text = str(score)+"/"+str(target_points)+" coins"
 	score_label_2.text = str(score)+"/"+str(target_points)+" coins"
 	score_label_3.text = str(score)+"/"+str(target_points)+" coins"
+	mainlabel.text = "Points:"+str(score)+"Level:"+str(level)
 
 func add_level():
 	level += 1
 	print(level)
+	mainlabel.text = "Points:"+str(score)+"Level:"+str(level)
 	
-
-
-func _on_level_boundary_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
--- susgame/scripts/level_boundary.gd
+++ susgame/scripts/level_boundary.gd
@@ -6,9 +6,9 @@ extends Area2D
 #func _on_body_exited(body: Node2D):
 	#print("aaazaa")
 	#game_manager.add_level()
-	##animation_player.play("pickup")
+	#animation_player.play("pickup")
 
-
+#
 func _on_body_entered(body: Node2D) -> void:
 	print("new level")
 	game_manager.add_level()
