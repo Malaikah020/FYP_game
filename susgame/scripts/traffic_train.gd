93555e5d7989444d2c5ce35d7c015abffb962001
Malaikah Hafeez <Malaikah Hafeez>
2025-02-05 15:05:28 +0000

    train fit to scene

--- susgame/scripts/traffic_train.gd
+++ susgame/scripts/traffic_train.gd
@@ -6,7 +6,7 @@ extends Node2D
 
 const SPEED = 60
 
-var direction = 1
+var direction = -1
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if ray_castright.is_colliding():
